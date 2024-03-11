dd3a96659c1c32d57661fabbe3f455449031cb1a
mehulfollytobevice <49192179+mehulfollytobevice@users.noreply.github.com>
2024-03-11 19:14:17 -0400

    added data validation python file

---
+++ src/config.py
@@ -0,0 +1,4 @@
+#  this is the configuration file, all the settings like model hyper parameters, storage links are stored here
+
+
+GCP_URL= 'https://storage.googleapis.com/price_alchemy/Data/data.csv'
