1242fd2f94baa6431c6700a6ba2fa394d51a90ed
mehulfollytobevice <49192179+mehulfollytobevice@users.noreply.github.com>
2024-03-12 17:19:18 -0400

    data preprocessing changes

---
+++ src/price_alchemy/data_preprocessing.py
@@ -0,0 +1,20 @@
+# import libraries
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+
+def data_prep_v1(df):
+
+    # convert text column to list of strings
+    raw_text= df['item_description'].to_list()
+
+
+if __name__=="__main__":
+
+    print('Preprocessing data file')
+
+
